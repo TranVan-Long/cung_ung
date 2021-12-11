e6f1c722628f4245a94a82043589b87137b47b6e
longtvph08895 <57783272+longtvph08895@users.noreply.github.com>
2021-12-11 13:56:50 +0700

    them moi

--- home/ql_trangchu.php
+++ home/ql_trangchu.php
@@ -7,8 +7,8 @@ include "../includes/icon.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Quản lý đơn hàng</title>
-    <link href="https://timviec365.vn/favicon.ico" rel="shortcut icon"/>
+    <title>Quản lý trang chủ</title>
+    <link href="https://timviec365.vn/favicon.ico" rel="shortcut icon" />
 
     <link rel="preload" href="../fonts/Roboto-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
     <link rel="preload" href="../fonts/Roboto-Medium.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
@@ -32,18 +32,163 @@ include "../includes/icon.php";
             </div>
 
             <div class="content">
-                <div class="ctt_tt_one">
-                    <div class="tt_left_one">
-                        <div class="til_fulln"></div>
+                <div class="ctt_tt_one w_100 fload_l">
+                    <div class="tt_left_one staff_fulln d_flex flex_jct">
+                        <div class="til_fulln">
+                            <h1 class="ten_nvien_ql  w_100 fload_l cr_weight"><span class="share_clr_one">Xin
+                                    chào,</span> <span class="share_clr_four">Nguyễn Văn A</span></h1>
+                            <p>Chúc bạn một ngày mới làm việc hiệu quả!</p>
+                        </div>
+                        <div class="titl_avt_full">
+                            <img src="../img/pana.png" alt="ảnh đại diện">
+                        </div>
                     </div>
-                    <div class="tt_right_one"></div>
+                    <div class="tt_right_one staff_fulln">
+                        <h3 class="time_tt cr_weight share_clr_one">08:00</h3>
+                        <p class="cr_weight share_clr_one share_fsize_tow">Thứ ba, 11/05/2021</p>
+                    </div>
                 </div>
-                <div class="ctt_tt_two">
-                    <div class="tt_left_tow"></div>
-                    <div class="tt_left_tow"></div>
+                <div class="ctt_tt_two mb_20 w_100 fload_l">
+                    <div class="tt_left_tow staff_fulln">
+                        <div class="tieu_de_bd w_100 fload_l d_flex mb_20 flex_jct">
+                            <h3 class="tieu_de_ct share_fsize_four share_clr_one mb_10">Công nợ phải thu</h3>
+                            <div class="filter_mdy">
+                                <select name="tim_kiem" class="form_search_dmy">
+                                    <option value="1">Theo ngày</option>
+                                    <option value="2">Theo tháng</option>
+                                    <option value="3">Theo năm</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="chart"></div>
+                        <div class="ttin_ctiet w_100 fload_l mb_20">
+                            <div class="tcong_no w_100 fload_l d_flex mb_10 fl_wrap flex_jct">
+                                <p class="share_fsize_tow share_clr_one">Tổng công nợ</p>
+                                <p class="share_fsize_tow share_clr_one cr_weight">1.000.000.000</p>
+                            </div>
+                            <div class="da_thu w_100 fload_l d_flex mb_10 fl_wrap flex_jct">
+                                <p class="dthu share_fsize_tow share_clr_one">Đã thu:</p>
+                                <p class="share_fsize_tow cr_xanh_dam cr_weight">100.000.000</p>
+                            </div>
+                            <div class="con_thu w_100 fload_l d_flex mb_10 fl_wrap flex_jct">
+                                <p class="cphai_thu share_fsize_tow share_clr_one">Còn phải thu</p>
+                                <p class="share_fsize_tow cr_vang cr_weight">900.000.000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tt_left_tow staff_fulln tt_right_tow fload_l">
+                        <div class="tieu_de_bd w_100 fload_l d_flex mb_20 flex_jct">
+                            <h3 class="tieu_de_ct share_fsize_four share_clr_one mb_10">Công nợ phải trả</h3>
+                            <div class="filter_mdy">
+                                <select name="tim_kiem" class="form_search_dmy">
+                                    <option value="1">Theo ngày</option>
+                                    <option value="2">Theo tháng</option>
+                                    <option value="3">Theo năm</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="charts"></div>
+                        <div class="ttin_ctiet w_100 fload_l mb_20">
+                            <div class="tcong_no w_100 fload_l d_flex mb_10 fl_wrap flex_jct">
+                                <p class="share_fsize_tow share_clr_one">Tổng công nợ</p>
+                                <p class="share_fsize_tow share_clr_one cr_weight">1.000.000.000</p>
+                            </div>
+                            <div class="da_thu w_100 fload_l d_flex mb_10 fl_wrap flex_jct">
+                                <p class="dtra share_fsize_tow share_clr_one">Đã trả:</p>
+                                <p class="share_fsize_tow cr_da_cam cr_weight">100.000.000</p>
+                            </div>
+                            <div class="con_thu w_100 fload_l d_flex mb_10 fl_wrap flex_jct">
+                                <p class="cphai_tra share_fsize_tow share_clr_one">Còn phải trả:</p>
+                                <p class="share_fsize_tow cr_do_nhat cr_weight">900.000.000</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="ctt_tt_three">
-                    <div class="ctn_tt_three"></div>
+                <div class="ctt_tt_three w_100">
+                    <div class="tieu_de_chart w_100 fload_l d_flex">
+                        <h3 class="tieu_de_ct share_fsize_four share_clr_one mb_10">Giá trị hợp đồng mua, bán</h3>
+                        <div class="search_chart">
+                            <select name="search_chart" class="form_search_dmy share_clr_one share_fsize_tow">
+                                <option value="1">Theo năm</option>
+                                <option value="2">Theo tháng</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="content_hcot w_100 fload_l">
+                        <div id="tt_three_ctiet"></div>
+                        <table id="datatable" class="share_dnone">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Hợp đồng mua</th>
+                                    <th>Hợp đồng bán</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th></th>
+                                    <td>3</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>5</td>
+                                    <td>11</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>1</td>
+                                    <td>19</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>50</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <td>2</td>
+                                    <td>4</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,9 +196,110 @@ include "../includes/icon.php";
 </body>
 <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
 <script src="../js/select2.min.js"></script>
-<script type="text/javascript" src="../js/sidebar-accordion.js"></script>
-<script>
+<script src="../js/apexcharts.min.js"></script>
 
+<!-- js bieu do cot -->
+<script src="../js/highcharts.js"></script>
+<script src="../js/export-data.js"></script>
+<script src="../js/exporting.js"></script>
+<script src="../js/accessibility.js"></script>
+<!-- end -->
+<script type="text/javascript" src="../js/sidebar-accordion.js"></script>
+
+<script>
+    var options = {
+        series: [44000006, 5500000],
+        chart: {
+            width: 280,
+            type: 'pie',
+        },
+        colors: ['#219653', '#F2C94C'],
+        labels: ['Đã thu', 'Chưa thu'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 160
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+    var chart = new ApexCharts(document.querySelector(".chart"), options);
+    chart.render();
+</script>
+<script>
+    var options = {
+        series: [4400000, 55000000],
+        chart: {
+            width: 280,
+            type: 'pie',
+        },
+        colors: ['#F2994A', '#EB5757'],
+        labels: ['Đã thu', 'Chưa thu'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+    var chart = new ApexCharts(document.querySelector(".charts"), options);
+    chart.render();
+</script>
+<script>
+    Highcharts.chart('tt_three_ctiet', {
+        data: {
+            table: 'datatable'
+        },
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+
+        yAxis: {
+            allowDecimals: false,
+            title: {
+                text: ''
+            },
+            labels: {
+                style: {
+                    color: '#474747',
+                    fontSize: '14px',
+                }
+            },
+        },
+        tooltip: {
+            style: {
+                color: '#474747',
+                fontSize: '14px',
+            }
+        },
+
+        xAxis: {
+            labels: {
+                style: {
+                    color: '#474747',
+                    fontSize: '14px',
+                }
+            },
+
+            categories: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8',
+                'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'
+            ],
+        },
+
+        colors: ['#E09A6A', '#9D92C8'],
+    });
 </script>
 
 </html>
